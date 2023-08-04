b1b8f9f7e4dd8d366acd1a75afb334d14fe1c576
Leonardo Abdala <leonardo-abdala@hotmail.com>
2023-08-04 08:44:40 -0300

    Implementada parte 1 do projeto, com a classe Dolar e o seu teste

---
+++ src/atividadeengenharia2/Dolar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividadeengenharia2;
+
+/**
+ *
+ * @author Leonardo Abdala
+ */
+class Dollar {
+
+        int amount = 10;
+        Dollar(int amount) {}			
+        void times(int multiplier) {}
+    }
---
+++ test/atividadeengenharia2/DollarTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividadeengenharia2;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Leonardo Abdala
+ */
+public class DollarTest {
+    
+    public DollarTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of times method, of class Dollar.
+     */
+    @Test
+    public void testMultiplication() {
+       Dollar five = new Dollar(5);
+       five.times(2);
+       assertEquals(10, five.amount);
+    }
+    
+}
